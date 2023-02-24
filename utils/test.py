077a8f697c73f330fa3b83e2d4950b53e4f2fb78
Harsh Sharma <63489899+harshvs4@users.noreply.github.com>
2023-02-24 14:58:18 +0530

    Update test.py

--- utils/test.py
+++ utils/test.py
@@ -1,25 +1,68 @@
 import torch
-import torch.nn.functional as F
-import torch.nn as nn
 
+class Test:
+    def __init__(self, model, test_loader, criterion, device) -> None:
+        self.test_losses = []
+        self.test_accuracies = []
+        self.model = model.to(device)
+        self.test_loader = test_loader
+        self.criterion = criterion
+        self.device = device
 
-def test(model, device, test_loader,test_acc,test_losses,criterion):
-    model.eval()
-    test_loss = 0
-    correct = 0
-    with torch.no_grad():
-        for data, target in test_loader:
-            data, target = data.to(device), target.to(device)
-            output = model(data)
-            test_loss += criterion(output, target).item()  # sum up batch loss
-            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
-            correct += pred.eq(target.view_as(pred)).sum().item()
+    def test(self):
+        self.model.eval()
 
-    test_loss /= len(test_loader.dataset)
-    test_losses.append(test_loss)
+        test_loss = 0
+        correct = 0
 
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
-    
-    test_acc.append(100. * correct / len(test_loader.dataset))
+        with torch.no_grad():
+            for inputs, targets in self.test_loader:
+                inputs, targets = inputs.to(self.device), targets.to(self.device)
+                output = self.model(inputs)
+                loss = self.criterion(output, targets)
+
+                test_loss += loss.item()
+
+                pred = output.argmax(
+                    dim=1, keepdim=True
+                )  # get the index of the max log-probability
+                correct += pred.eq(targets.view_as(pred)).sum().item()
+
+        test_loss /= len(self.test_loader.dataset)
+        self.test_losses.append(test_loss)
+
+        print(
+            "Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)".format(
+                test_loss,
+                correct,
+                len(self.test_loader.dataset),
+                100.0 * correct / len(self.test_loader.dataset),
+            )
+        )
+
+        self.test_accuracies.append(100.0 * correct / len(self.test_loader.dataset))
+
+        return 100.0 * correct / len(self.test_loader.dataset), test_loss
+
+    def get_misclassified_images(self):
+        self.model.eval()
+
+        images = []
+        predictions = []
+        labels = []
+
+        with torch.no_grad():
+            for data, target in self.test_loader:
+                data, target = data.to(self.device), target.to(self.device)
+
+                output = self.model(data)
+
+                _, preds = torch.max(output, 1)
+
+                for i in range(len(preds)):
+                    if preds[i] != target[i]:
+                        images.append(data[i])
+                        predictions.append(preds[i])
+                        labels.append(target[i])
+
+        return images, predictions, labels
